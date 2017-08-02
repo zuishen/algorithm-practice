d586566ce419de6f59fdb014c65af2ad65dc58d9
Zui Shen <zshen.usc@gmail.com>
2017-08-02 12:11:19 -0700

    Update palindromeNumber.java

--- 9-PalindromeNumber/palindromeNumber.java
+++ 9-PalindromeNumber/palindromeNumber.java
@@ -1,3 +1,9 @@
+/*  
+*  For this question:
+*  1. Could negative integers be palindromes? No
+*  2. Overflow?  (1) 2147483647最大范围内的palindrome位21474 47412，再大则不是，若继续增长则发生溢出，
+*     又因为题目参数是int，因此溢出可以一律视为不是。 
+*/
 public class Solution {
     public boolean palindromeNumber(int x) {
         if (x < 0) {
@@ -8,7 +14,7 @@ public class Solution {
         while (num != 0) {
             int digit = num % 10;
             //int tmp = ret;
-            int ret = 10 * ret + digit;
+            ret = 10 * ret + digit;
             /*
             if (tmp != ret / 10) {
                 return false;
