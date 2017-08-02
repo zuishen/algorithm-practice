b0f1a662ea48ed794a50ac8facfa254a909de38b
Zui Shen <zshen.usc@gmail.com>
2017-08-01 18:59:37 -0700

    Update reverseInteger.java

--- 7-ReverseInteger/reverseInteger.java
+++ 7-ReverseInteger/reverseInteger.java
@@ -11,8 +11,9 @@ public class Solution {
             /* 与2147483647同样数位时，最高位一定是<=2，而2147483640+8才会造成溢出，这意味着input为8463847412，
                因此此时所加的digit不会造成溢出。 if (tmp > (Integer.MAX_VALUE - digit) / 10)  --> 在符号一定相同情况下才可使用！
                                         或者 if (tmp != ret / 10)   --> 相除后不会保留各位 所以结果应当相同
+                                        或者 if (tmp != (ret - digit) / 10) 
             */
-                return 0；
+                return 0;
             }
             x = x / 10;
         }
