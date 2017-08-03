e1fa5af6fd6585623c1779a4a0eb5704fe14dc0e
Zui Shen <zshen.usc@gmail.com>
2017-08-03 14:27:41 -0700

    Create lcp.java

---
+++ 14-LongestCommonPrefix/lcp.java
@@ -0,0 +1,8 @@
+/*
+* Write a function to find the longest common prefix string among an array of strings
+*/
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+    }
+}
