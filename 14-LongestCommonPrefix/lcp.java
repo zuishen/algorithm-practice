a6087aa2a29b8f8eb3f8f99b8ed5539610a08471
Zui Shen <zshen.usc@gmail.com>
2017-08-03 14:40:31 -0700

    Update lcp.java

--- 14-LongestCommonPrefix/lcp.java
+++ 14-LongestCommonPrefix/lcp.java
@@ -4,5 +4,16 @@
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {        //strs[0]的每一个字符
+            for (int j = 1; j < strs.length; j++) {         //分别与数组中的字符串的相应字符进行比较
+                if (strs[j].length() == i || strs[0].charAt(i) != strs[j].charAt(i)) {  //注意字符串长度
+                    return strs[0].substring(0, i);   
+                }
+            }
+        }
+        return strs[0];
     }
 }
