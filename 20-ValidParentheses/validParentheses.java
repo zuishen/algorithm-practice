deae999d1a0fd99e30468b5036e6caacb5cac93f
Zui Shen <zshen.usc@gmail.com>
2017-08-03 15:35:32 -0700

    Update validParentheses.java

--- 20-ValidParentheses/validParentheses.java
+++ 20-ValidParentheses/validParentheses.java
@@ -1,1 +1,33 @@
+/*
+*   Given a string containing just the characters (){}[],determine if the input string is valid.
+*   The brackets must close in the correct order.
+*/
 
+public class Solution {
+    public boolean validParentheses(String s) {
+        if (s == null) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char curt = s.charAt(i);
+            if (curt == ']' || curt == '}' || curt == ')') {
+                if (stack.empty()) return false;
+                char tmp = stack.pop();
+                switch (curt) {
+                    case ']': if (tmp != '[') return false;
+                              break;
+                    case '}': if (tmp != '{') return false;
+                              break;
+                    case ')': if (tmp != '(') return false;
+                              break;
+                    default: return false;      // 当字符串包含除{}[]()之外的字符串时返回false
+                }
+            } else {
+                stack.push(curt);   
+            }
+        }
+        if (!stack.empty()) return false;
+        return true;
+    }
+}
