837c9ae6a00a285778a88a2603e177814ca2a21c
Zui Shen <zshen.usc@gmail.com>
2017-08-08 16:14:43 -0700

    Update addBinary.java

--- 67-AddBinary/addBinary.java
+++ 67-AddBinary/addBinary.java
@@ -1,1 +1,14 @@
+/*
+*   Given two binary strings, return their sum(also a binary string).
+*/
 
+public class Solution {
+    public String addBinary(String a, String b) {
+    
+    }
+}
+
+/*
+*   和plus one有些相似。考虑的情况有："", ""; "1", ""; "11111", "11"; "1100", "11";
+*   ** 注意两个数组长度，当其中一个长度结束后，值为0。
+*/
