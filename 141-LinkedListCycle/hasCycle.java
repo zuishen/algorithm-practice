5fbbed0ec226969db721f736d85f519b59ec993a
Zui Shen <zshen.usc@gmail.com>
2017-08-16 00:31:13 -0700

    Update hasCycle.java

--- 141-LinkedListCycle/hasCycle.java
+++ 141-LinkedListCycle/hasCycle.java
@@ -1,1 +1,35 @@
+/*
+*   Given a linked list, determine if it has a cycle in it.
+*/
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle1(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = slow.next;
+        while (fast != null && fast.next != null) {
+            if (fast == slow) return true;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+    
+    public boolean hasCycle2(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) return true;
+            head = head.next;
+        }
+        return false;
+    }
+}
