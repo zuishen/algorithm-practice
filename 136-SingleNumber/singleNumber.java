f6f819f7c29706fa8d9d097090c2697dfe76222a
Zui Shen <zshen.usc@gmail.com>
2017-08-15 23:59:19 -0700

    Update singleNumber.java

--- 136-SingleNumber/singleNumber.java
+++ 136-SingleNumber/singleNumber.java
@@ -1,1 +1,48 @@
+/*
+*   Given an array of integers, every element appears twice except for one. Find that single one.
+*   linear runtime complexity, without using extra memory.
+*/
 
+public class Solution {
+    public int singleNumber1(int[] nums) {  // hashMap
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) return entry.getKey();
+        }
+        return 0;
+    }
+    
+    public int singleNumber2(int[] nums) {  // xor
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int i : nums) {
+            result = result ^ i;
+        }
+        return result;
+    }
+    
+    public int singleNumber3(int[] nums) {  // set
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) {
+                set.remove(i);
+            }
+        }
+        return set.iterator().next();
+    }
+}
