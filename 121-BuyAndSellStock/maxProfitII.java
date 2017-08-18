34e1a3d6d72062b72c220efa609ecd77168c2e59
Zui Shen <zshen.usc@gmail.com>
2017-08-17 22:35:37 -0700

    Update maxProfitII.java

--- 121-BuyAndSellStock/maxProfitII.java
+++ 121-BuyAndSellStock/maxProfitII.java
@@ -1,1 +1,6 @@
-
+/*
+*   Say you have an array for which the ith element is the price of a given stock on day i.
+*   Design an algorithm to find the maximum profit. You may complete as many transactions as
+*   you like (buy one and sell one share of the stock multiple times). However, you may not engage 
+*   in multiple transactions at the same time (you must sell the stock before you buy again)
+*/
