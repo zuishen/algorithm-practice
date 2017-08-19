31a60cc23449d6403d465107b1384f132d2076c5
Zui Shen <zshen.usc@gmail.com>
2017-08-18 21:39:28 -0700

    Update maxProfitII.java

--- 121-BuyAndSellStock/maxProfitII.java
+++ 121-BuyAndSellStock/maxProfitII.java
@@ -4,3 +4,29 @@
 *   you like (buy one and sell one share of the stock multiple times). However, you may not engage 
 *   in multiple transactions at the same time (you must sell the stock before you buy again)
 */
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        int peak = 0;
+        int vally = 0;
+        int sum = 0;
+        
+        while (i < prices.length) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
+                i++;
+            }
+            vally = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1]) {
+                i++;
+            }
+            peak = prices[i];
+            i++;
+            sum += peak - vally;
+        }
+        return sum;
+    }
+}
