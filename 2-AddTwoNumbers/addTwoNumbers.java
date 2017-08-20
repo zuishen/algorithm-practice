50d4549f4f2d59506449701283d3f2c94414f31b
Zui Shen <zshen.usc@gmail.com>
2017-08-19 20:04:42 -0700

    Update addTwoNumbers.java

--- 2-AddTwoNumbers/addTwoNumbers.java
+++ 2-AddTwoNumbers/addTwoNumbers.java
@@ -6,3 +6,34 @@
 *
 *   (2->4->3) + (5->6->4) = (7->0->8)
 */
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = l2;
+        ListNode prev = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null) {
+            int tmp1 = l1 == null ? 0 : l1.val;
+            int tmp2 = l2 == null ? 0 : l2.val;
+            int digit = tmp1 + tmp2 + carry;
+            carry = digit / 10;
+            digit = digit % 10;
+            
+            if (l1 != null) {
+                l1.val = digit;
+                prev = l1;
+                l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+            } else {
+                l2.val = digit;
+                prev.next = l2;
+                prev = l2;
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) prev.next = new ListNode(carry);
+        return dummy.next;
+    }
+}
