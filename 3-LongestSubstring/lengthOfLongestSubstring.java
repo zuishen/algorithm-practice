a6228121351d9a4455c94b780290a7213d2694ec
Zui Shen <zshen.usc@gmail.com>
2017-08-20 21:09:38 -0700

    Update lengthOfLongestSubstring.java

--- 3-LongestSubstring/lengthOfLongestSubstring.java
+++ 3-LongestSubstring/lengthOfLongestSubstring.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = 1;
+        Set<Character> set = new HashSet<>();
+        set.add(s.charAt(0));
+        int maxLength = 1;
+        
+        while (end < s.length()) {
+            if (!set.add(s.charAt(end))) {
+                while (start < end) {
+                    if (s.charAt(start) == s.charAt(end)) {
+                        start++;
+                        break;
+                    } else {
+                        set.remove(s.charAt(start));
+                        start++;
+                    }
+                }
+            }
+            if (maxLength < end - start + 1) {
+                maxLength = end - start + 1;
+            }
+            end++;
+        }
+        return maxLength;
+    }
+}
