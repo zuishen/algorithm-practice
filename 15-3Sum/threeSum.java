d7e4e407c2ff14518d1ad6bf0a740d8f6932d53b
Zui Shen <zshen.usc@gmail.com>
2017-08-26 00:15:39 -0700

    Update threeSum.java

--- 15-3Sum/threeSum.java
+++ 15-3Sum/threeSum.java
@@ -15,6 +15,43 @@
 // 那么如何跳过相同的数？ 可以将数组排序
 // 又因为数组是有序的了，所以可以用双指针来做
 
-
-
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            addResult(i + 1, -1 * nums[i], nums, result);
+            while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return result;
+    }
+    
+    private void addResult(int i, int target, int[] nums, List<List<Integer>> ret) {
+        int j = nums.length - 1;
+        while (i < j) {
+            if (target - nums[i] == nums[j]) {
+                List<Integer> tripple = new ArrayList<>(); // add() add the element to the end of the list
+                tripple.append(-target);
+                tripple.append(nums[i]);
+                tripple.append(nums[j]);
+                ret.add(tripple);
+                
+                i++;
+                j--;
+                while (i < j && nums[i - 1] == nums[i]) i++;
+                while (i < j && nums[j + 1] == nums[j]) j--;
+            } else if (target - nums[i] > nums[j]){
+                i++;
+            } else {
+                j--;
+            }
+        }
+    }
+}
+            
 
