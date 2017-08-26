ab1c9d5812b24deec111f58085a4625d3266abd3
Zui Shen <zshen.usc@gmail.com>
2017-08-25 22:43:14 -0700

    Update twoSum2.java

--- 1-TwoSum/twoSum2.java
+++ 1-TwoSum/twoSum2.java
@@ -25,6 +25,40 @@ public class Solution {
         }
         return null;
     }
+    
+    public int[] twoSumBinarySearch(int[] numbers, int target) {
+        if (numbers == null) {
+            return null;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            int tmp = binarySearch(i + 1, target - numbers[i], numbers);
+            if (tmp > 0) {
+                return new int[]{i + 1, tmp + 1};
+            }
+        }
+        return null;
+    }
+    
+    private int binarySearch(int start, int target, int[] num) {
+        int end = num.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (target == num[mid]) {
+                return mid;
+            } else if (target > num[mid]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (target == num[start]) {
+            return start;
+        }
+        if (target == num[end]) {
+            return end;
+        }
+        return - 1;
+    }
 }
         
         
