7881c85fdf6dde1000d9126a06cc23f09a3977f8
Zui Shen <zshen.usc@gmail.com>
2017-08-26 15:12:41 -0700

    Update threeSumClosest.java

--- 15-3Sum/threeSumClosest.java
+++ 15-3Sum/threeSumClosest.java
@@ -4,4 +4,28 @@
 *   [-1, 2, 1, -4], target = 1   ===>  (-1 + 2 + 1 = 2)
 */
 
-
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return -1;
+        }
+        Arrays.sort(nums);
+        int sum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int tmp = nums[i] + nums[start] + nums[end];
+                sum = Math.abs(target - tmp) < Math.abs(target - sum) ? tmp : sum;
+                if (tmp == target) {
+                    return tmp;
+                } else if (tmp < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        return sum;
+    }
+}
