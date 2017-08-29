e63edde6801f49d2f6b715a9a979cf81d6aab39f
Zui Shen <zshen.usc@gmail.com>
2017-08-28 21:03:02 -0700

    Update bubbleSort.java

--- basicSortAlgs/bubbleSort.java
+++ basicSortAlgs/bubbleSort.java
@@ -1,1 +1,16 @@
-
+    
+    private static int[] bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            boolean isSwap = false;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                    isSwap = true;
+                }
+            }
+            if (isSwap == false) return nums;
+        }
+        return nums;
+    }
