c6591e09f2f3dde8ffb26cfd6e0a86c15087a64f
Zui Shen <zshen.usc@gmail.com>
2017-08-28 21:04:27 -0700

    Update selectSort.java

--- basicSortAlgs/selectSort.java
+++ basicSortAlgs/selectSort.java
@@ -1,1 +1,12 @@
-s
+  private static int[] selectSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
+        }
+        return nums;
+    }
