6cc22d5d476eb81918277866447a8cfa308b6eff
Zui Shen <zshen.usc@gmail.com>
2017-08-28 21:05:46 -0700

    Update insertSort.java

--- basicSortAlgs/insertSort.java
+++ basicSortAlgs/insertSort.java
@@ -1,1 +1,13 @@
 
+private static int[] insertSort(int[] nums) {
+    for (int i = 1; i < nums.length; i++) {
+        int curr = nums[i];
+        int j = i - 1;
+        while (j >= 0 && curr < nums[j]) {
+            nums[j + 1] = nums[j];
+            j--;
+        }
+        nums[j + 1] = curr;
+    }
+    return nums;
+}
