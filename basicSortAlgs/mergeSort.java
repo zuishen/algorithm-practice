f8dd4aa5b938d943e36eb0fae6ce29551053e000
Zui Shen <zshen.usc@gmail.com>
2017-08-28 21:03:40 -0700

    Update mergeSort.java

--- basicSortAlgs/mergeSort.java
+++ basicSortAlgs/mergeSort.java
@@ -1,1 +1,29 @@
-
+    private static void mergeSort(int start, int end, int[] nums) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSort(start, mid, nums);
+        mergeSort(mid + 1, end, nums);
+        // merge
+        int[] tmp = new int[end - start + 1];
+        int left = start;
+        int right = mid + 1;
+        int i = 0;
+        while (left <= mid && right <= end) {
+            if (nums[left] <= nums[right]) {
+                tmp[i++] = nums[left++];
+            } else {
+                tmp[i++] = nums[right++];
+            }
+        }
+        while (left <= mid) {
+            tmp[i++] = nums[left++];
+        }
+        while (right <= end) {
+            tmp[i++] = nums[right++];
+        }
+        for (int j = 0; j < i; j++) {
+            nums[start + j] = tmp[j];
+        }
+    }
